6e6965d1268b4d4e842b53dcf587b2cdcc7ce5b5
Jit Kumar Saha <mail.jitsaha@gmail.com>
2026-06-13 02:17:31 +0600

    fix(setup): fix brace syntax and add URI routing rewrite

--- public/run-admin-setup.php
+++ public/run-admin-setup.php
@@ -152,7 +152,7 @@ if (!$symlinkCreated) {
         mkdir($apiSymlink, 0755, true);
     }
     $fallbackIndex = $apiSymlink . '/index.php';
-    $indexCode = "<?php\ndefine('LARAVEL_START', microtime(true));\nrequire '" . $apiDir . "/vendor/autoload.php';\n\$app = require_once '" . $apiDir . "/bootstrap/app.php';\n\$kernel = \$app->make(Illuminate\Contracts\Http\Kernel::class);\n\$response = \$kernel->handle(\n    \$request = Illuminate\Http\Request::capture()\n)->send();\n\$kernel->terminate(\$request, \$response);\n";
+    $indexCode = "<?php\ndefine('LARAVEL_START', microtime(true));\nif (isset(\$_SERVER['REQUEST_URI'])) {\n    \$_SERVER['REQUEST_URI'] = preg_replace('/^\\/api/', '', \$_SERVER['REQUEST_URI']);\n}\nrequire '" . $apiDir . "/vendor/autoload.php';\n\$app = require_once '" . $apiDir . "/bootstrap/app.php';\n\$kernel = \$app->make(Illuminate\Contracts\Http\Kernel::class);\n\$response = \$kernel->handle(\n    \$request = Illuminate\Http\Request::capture()\n)->send();\n\$kernel->terminate(\$request, \$response);\n";
     if (file_put_contents($fallbackIndex, $indexCode) !== false) {
         echo "<p style='color:green;'>Success: Created API physical folder router wrapper!</p>";
     } else {
