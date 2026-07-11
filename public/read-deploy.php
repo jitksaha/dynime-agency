27bba08d682cbe634517809df50e5f17e2a6dca4
Jit Kumar Saha <mail.jitsaha@gmail.com>
2026-07-11 17:31:51 +0600

    debug: add temporary read-deploy.php helper

---
+++ public/read-deploy.php
@@ -0,0 +1,22 @@
+<?php
+header('Content-Type: text/plain');
+if (!isset($_GET['token']) || $_GET['token'] !== 'deploy_token_7782') {
+    die('Unauthorized');
+}
+
+$files = [
+    'deploy-api.php',
+    '../deploy-api.php',
+    '../../deploy-api.php',
+    'public_html/deploy-api.php',
+];
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        echo "=== $file ===\n";
+        echo file_get_contents($file);
+        exit;
+    }
+}
+
+echo "deploy-api.php not found in: " . implode(', ', $files);
